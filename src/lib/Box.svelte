de72f2863180b8c79e75236cfd54f3ddabaf771c
Naufal <143169855+naufal135@users.noreply.github.com>
2024-05-07 22:46:15 +0700

    Init

---
+++ src/lib/Box.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+    export let taken: boolean;
+
+    const NOT_TAKEN_COLOR = "#15803d"
+    const TAKEN_COLOR = "#b91c1c"
+
+    $: color = taken ? TAKEN_COLOR : NOT_TAKEN_COLOR
+</script>
+
+<div class='aspect-[1/2.5] lg:aspect-[1/1.5] border-2 border-black' style:background-color={color}/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+    import { onMount } from "svelte";
+    import Box from "../lib/Box.svelte";
+
+    const url = 'http://65.108.223.40:8080'
+    $: data = [false, false, false, false, false, false]
+
+    onMount(() => {
+        async function fetchData() {
+            const res = await fetch(url)
+            data = await res.json()
+        }
+
+        const interval = setInterval(fetchData, 1000)
+
+        return () => clearInterval(interval)
+    })
+</script>
+
+<div class='h-screen w-screen grid grid-cols-6 place-content-center'>
+    {#each {length: 6} as _, i }
+        <Box taken={data[i]}/>
+    {/each}
+    {data}
+</div>
